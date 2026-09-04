467e82e5f3fce6fe3fe7727c1be36960db7d97d6
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2026-09-04 01:53:10 -0700

    Restrict Howdy to interactive login
    The configuration enables the general service but specifically allows
    Howdy only for the login service

--- modules/nixos/biometrics.nix
+++ modules/nixos/biometrics.nix
@@ -19,30 +19,6 @@
   };
 
   # Restrict Howdy to interactive login only
-  security.pam.services = {
-    chfn.howdy.enable = false;
-    chpasswd.howdy.enable = false;
-    chsh.howdy.enable = false;
-    cups.howdy.enable = false;
-    dms-greeter.howdy.enable = false;
-    greetd.howdy.enable = false;
-    groupadd.howdy.enable = false;
-    groupdel.howdy.enable = false;
-    groupmems.howdy.enable = false;
-    groupmod.howdy.enable = false;
-    login.howdy.enable = true;
-    passwd.howdy.enable = false;
-    polkit-1.howdy.enable = false;
-    runuser.howdy.enable = false;
-    runuser-l.howdy.enable = false;
-    su.howdy.enable = false;
-    sudo.howdy.enable = false;
-    swaylock.howdy.enable = false;
-    systemd-run0.howdy.enable = false;
-    systemd-user.howdy.enable = false;
-    useradd.howdy.enable = false;
-    userdel.howdy.enable = false;
-    usermod.howdy.enable = false;
-    vlock.howdy.enable = false;
-  };
+  security.pam.howdy.enable = false;
+  security.pam.services.login.howdy.enable = true;
 }
